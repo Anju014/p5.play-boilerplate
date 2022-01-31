bb083ee7be4d91972857600ef34cb2023d4d4d5b
Anju014 <84642983+Anju014@users.noreply.github.com>
2022-01-31 21:58:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
+function preload(){
+  
+}
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black");  
   drawSprites();
 }
